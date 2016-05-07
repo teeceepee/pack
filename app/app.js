2be43a5c84d549339a9a6595c1a768723c34e66b
Yu Haidong <yuhaidongyhd@gmail.com>
2016-05-07 17:29:16 +0800

    Add jQuery

--- app/app.js
+++ app/app.js
@@ -1,1 +1,14 @@
-console.log('app');
+var $ = require('jquery');
+
+$(document).ready(function () {
+  var index = $('.index-container');
+  index.html(
+    $('<h1>').text('jQuery powered')
+  );
+
+  for (let i of [1, 2]) {
+    index.fadeToggle();
+  }
+
+});
+
