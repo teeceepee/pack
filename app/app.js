6b815fae1825f197c01b422174cd28658f105c4e
Yu Haidong <yuhaidongyhd@gmail.com>
2016-05-07 17:36:33 +0800

    Add Lodash

--- app/app.js
+++ app/app.js
@@ -1,10 +1,15 @@
 var $ = require('jquery');
+var _ = require('lodash');
 
 $(document).ready(function () {
   var index = $('.index-container');
   index.html(
     $('<h1>').text('jQuery powered')
   );
+
+  _.times(2, function () {
+    index.slideToggle();
+  })
 
   for (let i of [1, 2]) {
     index.fadeToggle();
