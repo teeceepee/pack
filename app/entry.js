b5f067ce2ff61824dc24699494c334ce7e9a2d40
Yu Haidong <yuhaidongyhd@gmail.com>
2016-05-07 17:05:15 +0800

    Init commit

---
+++ app/entry.js
@@ -0,0 +1,3 @@
+require('./app');
+
+console.log('entry');
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  entry: "./app/entry.js",
+  output: {
+    path: __dirname + '/build',
+    filename: "bundle.js"
+  }
+};
+
