4e36b13ae04cacbdbe64b42e05f66fbf859eed72
Yu Haidong <yuhaidongyhd@gmail.com>
2016-05-08 15:45:43 +0800

    No need to use url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -21,11 +21,7 @@ module.exports = {
         loaders: ['style', 'css', 'sass']
       },
       {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "url-loader?limit=10000&minetype=application/font-woff"
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        test: /\.(woff|woff2|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         loader: "file-loader"
       }
     ]
