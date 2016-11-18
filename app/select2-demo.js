621eb1cece2fcf5a8b723cad6e369ab635338125
Yu Haidong <yuhaidongyhd@gmail.com>
2016-11-18 23:28:15 +0800

    Select2 dynamic options

--- app/select2-demo.js
+++ app/select2-demo.js
@@ -45,3 +45,38 @@ $("#array-data").select2({
 
 $("#multiple").select2({
 })
+
+const dynamicData = {
+  "": [
+    ""
+  ],
+  "Ruby": [
+    "Rails",
+    "Sinatra",
+    "Grape",
+  ],
+  "JavaScript": [
+    "React",
+    "Vue",
+  ]
+}
+
+$("#language").select2({
+  data: Object.keys(dynamicData),
+  minimumResultsForSearch: Infinity,
+}).on("change", function (e) {
+  var current = $(e.target).val()
+  var el = $("#framework")
+
+  // Select2 的 data 选项是插入新的 option 元素,
+  // 通过先清空 option 再插入实现重新设置选项
+  if (el.data("select2")) {
+    el.html("")
+  }
+
+  el.select2({
+    data: dynamicData[current],
+    minimumResultsForSearch: Infinity,
+  })
+
+}).trigger("change")
