88f326809d70144795513b784a7a3ea6defd9a6b
Yu Haidong <yuhaidongyhd@gmail.com>
2017-04-15 21:12:07 +0800

    Refactor export style

--- app/todo/actions/index.js
+++ app/todo/actions/index.js
@@ -1,5 +1,6 @@
 let nextTodoId = 0
-export const addTodo = (text) => {
+
+const addTodo = (text) => {
   return {
     type: 'ADD_TODO',
     id: nextTodoId++,
@@ -7,16 +8,18 @@ export const addTodo = (text) => {
   }
 }
 
-export const setVisibilityFilter = (filter) => {
+const setVisibilityFilter = (filter) => {
   return {
     type: 'SET_VISIBILITY_FILTER',
     filter
   }
 }
 
-export const toggleTodo = (id) => {
+const toggleTodo = (id) => {
   return {
     type: 'TOGGLE_TODO',
     id
   }
 }
+
+export { addTodo, setVisibilityFilter, toggleTodo }
